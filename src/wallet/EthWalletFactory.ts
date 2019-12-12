ea97077c25ae5b0361a71fd24ed9c79160801711
tkmct <1220t.takamichi@gmail.com>
2019-12-13 01:30:51 +0900

    Fix interface

--- src/wallet/EthWalletFactory.ts
+++ src/wallet/EthWalletFactory.ts
@@ -1,11 +1,9 @@
 import { wallet } from 'wakkanay'
-import { InMemoryKeyValueStore } from 'wakkanay/dist/db'
 import { EthWallet } from './EthWallet'
 import * as ethers from 'ethers'
 import IWallet = wallet.IWallet
 import IWalletFactory = wallet.IWalletFactory
 import Provider = ethers.providers.Provider
-import { Bytes } from 'wakkanay/dist/types'
 
 export class EthWalletFactory implements IWalletFactory {
   // Default provider will connect to eth main net
@@ -17,7 +15,6 @@ export class EthWalletFactory implements IWalletFactory {
   }
 
   async fromPrivateKey(privateKey: string): Promise<IWallet> {
-    const kvs = new InMemoryKeyValueStore(Bytes.fromString('kvs'))
-    return new EthWallet(new ethers.Wallet(privateKey, this.provider), kvs)
+    return new EthWallet(new ethers.Wallet(privateKey, this.provider))
   }
 }
