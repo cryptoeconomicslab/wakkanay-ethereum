2ab9ff0c9bc654dccdddc8ad52f25caa069678bf
tkmct <1220t.takamichi@gmail.com>
2020-01-07 23:27:50 +0900

    reexport as wakkanay

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import * as wakkanay from 'wakkanay'
+
 import * as coder from './coder'
 import * as contract from './contract'
 import * as events from './events'
 import * as wallet from './wallet'
 
-export { coder, contract, events, wallet }
+export { wakkanay, coder, contract, events, wallet }
